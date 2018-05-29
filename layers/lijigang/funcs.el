f942b3b48924a066c3923c941012e6af51990843
lijigang <i@lijigang.com>
2018-05-29 19:52:23 +0800

    modify font

--- layers/lijigang/funcs.el
+++ layers/lijigang/funcs.el
@@ -33,7 +33,7 @@
             headheight=15pt    % 标准中没有要求页眉的高度，这里设置成
                                % 15pt了
            ]{geometry}
-\\setCJKmainfont[BoldFont={STXihei},ItalicFont={KaiTi}]{STFangsong}
+\\setCJKmainfont[BoldFont={Microsoft YaHei},ItalicFont={Microsoft YaHei}]{Microsoft YaHei}
 "
                   ("\\section{%s}" . "\\section*{%s}")
                   ("\\subsection{%s}" . "\\subsection*{%s}")
