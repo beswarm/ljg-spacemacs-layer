31fbc62e3e8b061846f447dc4cdb4942d43154f6
lijigang <i@lijigang.com>
2018-10-22 17:36:22 +0800

    换个新主题皮肤

--- init.el
+++ init.el
@@ -126,6 +126,7 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         doom-molokai
                          monokai
                          molokai
                          dracula
